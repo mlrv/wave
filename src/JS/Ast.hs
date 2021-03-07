ae3185807efc05169f43e4e1fb7e852deee20541
marco <marco.levrero@gmail.com>
2021-03-07 17:38:18 +0000

    AST structure

---
+++ src/JS/Ast.hs
@@ -0,0 +1,48 @@
+module JS.Ast where
+
+import qualified Data.Map as M
+import qualified Data.Text as T
+
+--
+type Var = T.Text
+
+--
+type Record a =
+  M.Map Var a
+
+--
+data Lit
+  = LInt Int
+  | LBoool Bool
+  | LFloat Float
+  | LString T.Text
+  deriving (Show)
+
+--
+data Statement
+  = SExpr Expr
+  | SDef Definition
+  deriving (Show)
+
+--
+type Sub = [Statement]
+
+--
+data Expr
+  = ELit Lit
+  | EVar Var
+  | EFun [Var] Sub
+  | EFunCall Expr [Expr]
+  | ERecord (Record Expr)
+  deriving (Show)
+
+--
+data Definition
+  = Variable Var Expr
+  | Function Var [Var] Sub
+  deriving (Show)
+
+--
+data File
+  = File [Statement]
+  deriving (Show)
